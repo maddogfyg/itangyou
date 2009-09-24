1be3e6e1ad089f2407e5d5a8fb7e5d089e7289ba
maddogfyg <maddogfyg@gmail.com>
2009-09-24 15:58:40 +0800

    1

--- data/bbscache/usersort_judge.php
+++ data/bbscache/usersort_judge.php
@@ -6,5 +6,10 @@ $usersort_judge=array(
 	'onlinetime' => '0',
 	'postnum' => '0',
 	'rvrc' => '0',
+	'digests' => '0',
+	'f_num' => '0',
+	'monoltime' => '0',
+	'monthpost' => '0',
+	'todaypost' => '0',
 );
 ?>
